fe25127badbf0eb0af043d0af402adafe2cd502d
NatasyaAlia <natasyaalia436@gmail.com>
2025-03-18 16:44:12 +0700

    Membuat file lanjutan File 1

--- File1.cpp
+++ File1.cpp
@@ -17,5 +17,9 @@ int main(){
     cout << "Perulangan For Pencacah Turun" << endl;
     for (i = 4; i >= 0; i--){
         cout << "Nilai i : "<< i 
+        << "Lari keliling lapangan" << endl;
     }
-}
+
+    cout << "Nilai i terakhir : " << i << endl;
+    
+}   

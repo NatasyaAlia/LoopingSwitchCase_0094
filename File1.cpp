5aac68f6a59f5d51ae4231c9f7bd71e8555333cd
NatasyaAlia <natasyaalia436@gmail.com>
2025-03-18 17:20:39 +0700

    Perbaikan kode

--- File1.cpp
+++ File1.cpp
@@ -21,5 +21,7 @@ int main(){
     }
 
     cout << "Nilai i terakhir : " << i << endl;
+
+    
     
 }   
--- File2.cpp
+++ File2.cpp
@@ -3,3 +3,35 @@
 #include <ctime>
 using namespace std;
 
+int main (){
+    //Perulangan While
+    cout << "Perulangan While" << endl;
+    int x;
+    int PerulanganWhile = 0;
+    int PerulanganDoWhile = 0;
+    srand(time(0));
+    x = rand()% 10;
+
+    cout << "Nilai x awal : " << x << endl;
+    while(x < 7){
+        cout << "Nilai x : " << " Lari keliling Lapangan" << endl;
+        x = rand() % 10;
+        PerulanganWhile++;
+    }
+    cout << "Nilai x akhir : " << endl;
+    cout << "Jumlah perulangan : " << PerulanganWhile << endl;
+    cout << endl;
+
+    //Perulangan Do While
+    cout << "Perulangan Do While" << endl;
+    do{
+        cout << "Nilai x : " << x << " Lari keliling Lapangan" << endl;
+        x = rand() % 10;
+        PerulanganWhile++;
+      }while (x < 7);
+      cout << "Nilai x akhir : " << x << endl;
+      cout << "Jumlah perulangan ini saat : " << PerulanganDoWhile << endl;
+
+      
+
+}
